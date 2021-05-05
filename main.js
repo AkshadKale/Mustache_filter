43ba66224148debbb4a19cb29811e1884a2a54b2
AkshadKale <74201779+AkshadKale@users.noreply.github.com>
2021-05-05 21:01:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-function preload()
-{
+noseX = 0;
+noseY = 0;
 
+function preload(){
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup(){
@@ -13,25 +15,30 @@ function setup(){
     poseNet.on('pose' , gotPoses);
 }
 
-function modelLoaded(){
-    console.log('Pose Net is intialized');
+function modelLoaded()
+{
+    console.log("pose Net is intialized");
 }
 
 function draw()
 {
     image(video , 0 , 0 , 300 , 300);
+    image(mustache , noseX , noseY , 80 , 35);
 }
 
-function take_snapshot()
-{
-    save('MyFilterImage.png');
-}
 
 function gotPoses(results)
 {
     if(results.length > 0){
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 10;
+        noseY = results[0].pose.nose.y - 10;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
     }
+}
+
+function take_snapshot()
+{
+    save('MyFilterImage.png')
 }
